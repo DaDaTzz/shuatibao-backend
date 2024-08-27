300a97eba1261f1e2e1ac04ad6f17897db010f35
Da <1349190697@qq.com>
2024-08-27 10:01:35 +0800

    1

---
+++ src/main/java/com/da/shuatibao/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.da.shuatibao.mapper;
+
+import com.da.shuatibao.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * 
+ * 
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
---
+++ src/main/java/com/da/shuatibao/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.da.shuatibao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.da.shuatibao.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+ * 
+ * 
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
